c66f9381fbf056e844cb3cfad3a0ee835e35a387
Mohammad Mahdi Momeni <m.momeni@maahertsp.ir>
2023-12-23 17:59:48 +0330

    initial commit

---
+++ src/main/java/dev/momeni/appstatistics/AppStatisticsApplication.java
@@ -0,0 +1,13 @@
+package dev.momeni.appstatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppStatisticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppStatisticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/momeni/appstatistics/AppStatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.momeni.appstatistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppStatisticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
